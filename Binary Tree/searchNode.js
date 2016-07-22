ca8cd9f894540b7453172e4dff00acb3f5858881
ocamj015 <joaquin.x.ocampo.-nd@disney.com>
2016-07-22 17:53:16 -0300

    add logic to searchNode function

--- Binary Tree/searchNode.js
+++ Binary Tree/searchNode.js
@@ -8,7 +8,14 @@ function BinarySearchTree(){
     this.root = null;
 }
 
-var nodesArray = [];
+var nodesArray = [],
+    arr = [],
+    arrFinal = [],
+    nodoAct = 0,
+    index = 1,
+    corte = 1,
+    value = 0;
+
 
  /*
   * Generate unbalanced binary search tree
@@ -51,11 +58,6 @@ BinarySearchTree.prototype.push = function(val){
 }
 
 
-function searchNode() {
-    var value = document.getElementById("nodo").value;
-    if()
-}
-
 var bst = new BinarySearchTree();
 bst.push(40);
 bst.push(20);
@@ -65,6 +67,58 @@ bst.push(60);
 bst.push(50);
 bst.push(70);
 console.log(nodesArray);
+
+//arr.push(nodesArray[0].value);
+nodoAct = nodesArray[0];
+//console.log(arr);
+
+function searchNode() {
+    if(value === 0) {
+        value = parseInt(document.getElementById("nodo").value);
+    } 
+    // if (corte === 1) {
+    //     esIgual();
+    //}
+    if (value === nodoAct.value && arr[0] !== nodoAct.value) {
+        corte = 0;
+        return arr.push(nodoAct.value);
+    }        
+    // if (corte === 1) {
+    //     esMayor();
+    //     searchNode();
+    // }
+    if (value > nodoAct.value && nodoAct.right !== null) {
+        arr.push(nodoAct.value);
+        nodoAct = nodoAct.right;
+        return nodoAct;
+        searchNode();
+    }    
+    // if (corte === 1) {
+    //     arr = esMenor();
+    //     searchNode();
+    // }
+    if (value < nodoAct.value && nodoAct.left !== null) {
+        arr.push(nodoAct.value);
+        nodoAct = nodoAct.left;
+        return nodoAct;
+        searchNode();
+    }    
+
+    console.log(arr);
+}
+
+function esIgual() {
+
+}
+
+function esMayor() {
+
+}
+
+function esMenor() {
+
+}
+
 
 
  /*
